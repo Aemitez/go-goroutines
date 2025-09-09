39f0ac9b59b2406155f00847fa2bee99e7cd6cc7
Watcharapong <watcharapong_aem@hotmail.com>
2025-09-09 11:19:54 +0700

    feat: initial go and power point

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := range 3 {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+
+	// TODO example :No Goroutine (Sequential) vs  Goroutine
+	// exampleCompare("Normal")
+
+	// go exampleCompare("Power GO")
+
+	// time.Sleep(time.Millisecond)
+	// fmt.Println("done")
+
+	// *--------------------------------------------------------
+
+	// TODO example :2 Goroutine
+	// ch := make(chan int)
+
+	// go producer(ch) //  1 Goroutine
+	// go consumer(ch) //  2 Goroutine
+
+	// time.Sleep(time.Second)
+	// fmt.Println("done")
+
+	// *--------------------------------------------------------
+}
+
+func exampleCompare(state string) {
+	start := time.Now()
+
+	// ทำงานปกติ (รอ 2 วิ)
+	fmt.Printf("%s: Start working...\n", state)
+	time.Sleep(2 * time.Nanosecond)
+	fmt.Printf("%s: Work done!\n", state)
+
+	elapsed := time.Since(start)
+	fmt.Printf("%s:Finished in %s\n", state, elapsed)
+	fmt.Println("------------------------")
+}
+
+func producer(ch chan<- int) {
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("Producing: %d\n", i)
+		ch <- i
+		time.Sleep(100 * time.Millisecond)
+	}
+	close(ch) // Close the channel when done
+}
+
+func consumer(ch <-chan int) {
+	for num := range ch {
+		fmt.Printf("Consuming: %d\n", num)
+	}
+}
